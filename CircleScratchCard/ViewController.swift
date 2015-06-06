fb4a802c050ad9d5fa3685377147b94cdd082679
huangxiong <huangxionging@163.com>
2015-06-06 20:00:19 +0800

    Initial Commit

---
+++ CircleScratchCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleScratchCard
+//
+//  Created by huangxiong on 15/6/6.
+//  Copyright (c) 2015年 huangxiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
